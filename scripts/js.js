fc8050f9bf4722b45cca16811c187184afd42b17
Jesse Gonzalez <75717395+jgonz209@users.noreply.github.com>
2024-01-19 21:18:49 -0700

    Add existing website files

---
+++ scripts/js.js
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////////////////
+// Jesse Gonzalez                                                             //
+// La Marea Website                                                           //
+////////////////////////////////////////////////////////////////////////////////
+
+//declare variables
+var card1, card2, card3, card4, card5, card6;
+
+//find each card element and assign them to a variable
+card1 = document.getElementById("card1");
+card2 = document.getElementById("card2");
+card3 = document.getElementById("card3");
+card4 = document.getElementById("card4");
+card5 = document.getElementById("card5");
+card6 = document.getElementById("card6");
+
+//assign a class of "card" to each card
+card1.setAttribute("class", "card");
+card2.setAttribute("class", "card");
+card3.setAttribute("class", "card");
+card4.setAttribute("class", "card");
+card5.setAttribute("class", "card");
+card6.setAttribute("class", "card");
