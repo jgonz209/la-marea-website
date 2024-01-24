0feb395d77d6228a183f6a994dd873529a9f282b
jgonz209 <j.gonzalez6256@gmail.com>
2024-01-24 09:25:29 -0700

    Update logo image and refactor code

--- scripts/js.js
+++ scripts/js.js
@@ -3,25 +3,6 @@
 // La Marea Website                                                           //
 ////////////////////////////////////////////////////////////////////////////////
 
-//declare variables
-var card1, card2, card3, card4, card5, card6;
-
-//find each card element and assign them to a variable
-card1 = document.getElementById("card1");
-card2 = document.getElementById("card2");
-card3 = document.getElementById("card3");
-card4 = document.getElementById("card4");
-card5 = document.getElementById("card5");
-card6 = document.getElementById("card6");
-
-//assign a class of "card" to each card
-card1.setAttribute("class", "card");
-card2.setAttribute("class", "card");
-card3.setAttribute("class", "card");
-card4.setAttribute("class", "card");
-card5.setAttribute("class", "card");
-card6.setAttribute("class", "card");
-
 window.onbeforeunload = () => {
   window.scrollTo(0, 0);
 };
